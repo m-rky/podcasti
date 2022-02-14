a4d8f8f9637d0871dbedd8b53db061c86f7de5ea
Marky R <99497246+devMarky@users.noreply.github.com>
2022-02-13 19:31:57 -0500

    Updated dependencies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  webpack: (config) => {
-    // Unset client-side javascript that only works server-side
-    config.resolve.fallback = { fs: false, module: false };
-
-    return config;
-  },
   images: {
     domains: ["res.cloudinary.com"],
   },
