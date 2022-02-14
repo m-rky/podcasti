fb66c8996385febfc93b06c744f647d4be6d23e6
Marky R <99497246+devMarky@users.noreply.github.com>
2022-02-13 22:22:00 -0500

    Updated dependencies

--- pages/search/[param].js
+++ pages/search/[param].js
@@ -61,7 +61,7 @@ export default SearchPage;
 export async function getServerSideProps(context) {
   const query = context.query.param;
   const res = await search(context.query.param);
-  const { feeds, count } = await res;
+  const { feeds, count } = await res.json();
 
   if (feeds && count) {
     return {
